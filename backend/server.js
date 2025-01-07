0b83350de73e81fe9e589c7726e12417ddddaa8b
HenujaKa <katuwapitiyah@gmail.com>
2025-01-07 10:28:41 +0530

    Create the backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const {MongoClient, ObjectId} = require("mongodb")
+const cors = require("cors");
 const express = require("express")
 
 const app = express()
+
+app.use(express.json())
+app.use(cors())
 let db;
 
 app.get("/",async(req,res)=>{
@@ -40,31 +44,47 @@ app.get("/:id",async(req,res)=>{
             res.status(404).json("Not found");
         }
 })
-app.post("/users/add",async(req,res)=>{
+app.post('/admin/user/add', async (req, res) => {
+    const user = req.body;
     try {
-        const blogData = req.body;
-    
-        // Validate the input
-
-        if (!blogData || typeof blogData !== 'object') {
-            console.log(blogData)
-          return res.status(400).json({ message: "Invalid input data" });
-        }
-        console.log(blogData)
-        // Insert into the database
-        const result = await db.collection('users').insertOne(blogData);
-    
-        res.status(201).json({
-          message: "Blog added successfully",
-          blogId: result.insertedId,
-        });
-      } catch (error) {
-        console.error("Error adding blog:", error);
-        res.status(500).json({ message: "Internal Server Error", error: error.message });
-      }
-})
+        const result = await db.collection('users').insertOne(user);
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+app.post('/admin/Blogs', async (req, res) => {
+    const Blog = req.body;
+    console.log(Blog)
+    try {
+        const result = await db.collection('blogs').insertOne(Blog);
+        res.status(200).send(Blog);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
   
+app.put('/admin/Blogs/:sid', async (req, res) => {
+    const  sid  = parseInt(req.params.sid);
+    const updatedBlog = req.body;
 
+    try {
+        await db.collection('blogs').updateOne({ "ID":sid }, { $set: updatedBlog });
+        res.send(updatedBlog);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.delete('/admin/deleteBlog/:sid', async (req, res) => {
+    const deleteBlog =  await db.collection("blogs").deleteOne({"ID":parseInt(req.params.sid)});
+        if (deleteBlog) {
+            res.status(200).json("You have deletd Successfully");
+            console.log(deleteBlog);
+        }else{
+            res.status(404).json("Not found");
+        }
+});
 
 app.listen(8000 , async(req,res)=>{
     const client = new MongoClient("mongodb+srv://Henuja:Henuja123@blog.i5clm.mongodb.net/")
